c3c241a514055b938af7fe0a6ce3272d39412f40
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-30 23:52:49 -0700

    last-commit

---
+++ src/Components/AddUser.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const AddUser = () => {
+    const [user, setUser] = useState({});
+    const handleAddUser = event => {
+        event.preventDefault();
+        console.log(user);
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    alert('User added successfully');
+                    event.target.reset();
+                }
+            })
+    }
+
+    const handleInputBlur = event => {
+        const value = event.target.value;
+        const field = event.target.name;
+        // console.log(value, field)
+        const newUser = { ...user };
+        newUser[field] = value;
+        setUser(newUser);
+        console.log(newUser);
+    }
+
+    return (
+        <div>
+            <h3>Please add a new user.</h3>
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleInputBlur} type="text" name='name' placeholder='name' required />
+                <br />
+                <input onBlur={handleInputBlur} type="text" name='address' placeholder='address' required />
+                <br />
+                <input onBlur={handleInputBlur} type="email" name='email' placeholder='email' required />
+                <br />
+                <button type='submit'>Add User</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
